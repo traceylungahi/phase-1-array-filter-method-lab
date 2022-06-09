f967dde086e86d71abbd5c1d4109e8f628a7b056
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-09 21:53:26 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+const findMatching = (drivers, string) => {
+    const newDriver = [];
+    for(const name of drivers) {
+        if(name === string || name.toLowerCase() === string.toLowerCase()) {
+            newDriver.push(name)
+        } 
+    } 
+    return newDriver;
+}
+const fuzzyMatch = (drivers, strings) => { 
+    const driver = [];
+    for(const name of drivers) {
+        if(name.slice(0, 1) === strings.slice(0, 1)) {
+            driver.push(name)
+        }
+    }
+    return driver;
+}    
+const matchName = (drivers, string) => {
+    const newName = [];
+    for(const name of drivers) {
+        if(name.name === string || name.hometown === string) {
+        if(name.name.toLowerCase() === string.toLowerCase() || name.hometown.toLowerCase() === string.toLowerCase()) {
+            newName.push(name)
+        }  
+        }
+    }
+    return newName;
+}

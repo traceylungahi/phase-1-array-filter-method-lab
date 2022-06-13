dbd719f878b6cdece59761d05b81a6f655d5fd6c
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-13 09:05:39 +0300

    Done.

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const findMatching = (drivers, string) => {
         return newDrivers.toLowerCase() === string.toLowerCase()
     });
 }
-const fuzzyMatch = (drivers, strings) => {
+const fuzzyMatch = (drivers, string) => {
     return drivers. filter(function(newDrivers){
         return newDrivers.slice(0, 1) === string.slice(0, 1)
     });
 }
 const matchName = (drivers, string) => {
     return drivers.filter(function(newDrivers){
-        return driver.name === string
+        return newDrivers.name === string
     });
 }
     
